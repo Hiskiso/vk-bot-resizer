97cccf5b0fa11195cd5a2a2ca505ed557b4649d7
Roman <roma123qq@gmail.com>
2021-09-24 10:46:08 +0400

    Добавьте файлы проекта.

---
+++ VK longpoll bot resizer/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace VK_longpoll_bot_resizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(515, 511);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(771, 511);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(479, 532);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(643, 520);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(73, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Auto Start";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(722, 520);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(37, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "UI";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(515, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(378, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(905, 556);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Server ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ VK longpoll bot resizer/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using VkNet;
+
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Net;
+using System.IO;
+
+namespace VK_longpoll_bot_resizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> lines = new List<string>();
+        private bool GetUpdates = true;
+        private string AccessToken;
+        private string GroupId;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            Thread BotThr = new Thread(apiBotStart);
+
+            button1.Enabled = false;
+
+            BotThr.Start();
+        }
+
+        private void apiBotStart()
+        {
+            Log("Start bot...");
+
+            GetUpdates = true;
+
+            var api = new VkApi();
+
+            api.Authorize(new VkNet.Model.ApiAuthParams() { AccessToken = AccessToken });
+
+            
+            
+
+           
+
+            Log("Started.");
+
+
+
+            while (GetUpdates)
+            {
+                try
+                {
+                    var s = api.Groups.GetLongPollServer(ulong.Parse(GroupId));
+                    var poll = api.Groups.GetBotsLongPollHistory(
+                     new VkNet.Model.RequestParams.BotsLongPollHistoryParams() { Server = s.Server, Ts = s.Ts, Key = s.Key, Wait = 25 });
+
+                    if (poll?.Updates == null) continue;
+
+                    else
+                    {
+
+                        foreach (var update in poll.Updates)
+                        {
+                            if (update?.MessageNew.Message.Text == null) continue;
+
+                            else
+                            {
+                                string[] argsmessage = update.MessageNew.Message.Text.Split('.');
+
+                                if (update.Type == VkNet.Enums.SafetyEnums.GroupUpdateType.MessageNew)
+                                {
+
+                                    if (update?.MessageNew?.Message.ReadState == null)
+                                    {
+                                        Log(update.MessageNew.Message.FromId + ": " + update.MessageNew.Message?.Text);
+
+
+                                        if (update.MessageNew.Message.Attachments.Count == 0) continue;
+                                        else
+                                        {
+
+                                            VkNet.Model.Attachments.Photo photo = ((VkNet.Model.Attachments.Photo)update.MessageNew.Message.Attachments[0].Instance);
+
+                                            Log("Loading... " + photo.Sizes.Last().Url);
+
+
+                                            using(WebClient client = new WebClient())
+                                            {
+                                                client.DownloadFile(photo.Sizes.Last().Url, @"photo" + update.MessageNew.Message.Attachments[0].Instance.ToString() + ".png");
+                                            }
+                                           
+                                           
+
+                                            Log("photo" + update.MessageNew.Message.Attachments[0].Instance.ToString() + ".png   Saved.");
+
+                                            Log("Trasforming");
+
+                                            
+                                                Image img = new Bitmap("photo" + update.MessageNew.Message.Attachments[0].Instance.ToString() + ".png");
+
+                                                Image resized = new Bitmap(img, Int32.Parse(argsmessage[0]), Int32.Parse(argsmessage[0]));
+
+                                           
+
+                                                resized.Save("resized.png");
+
+                                                var upladServer = api.Photo.GetMessagesUploadServer((long)update.MessageNew.Message.PeerId);
+
+                                                var response = new WebClient().UploadFile(upladServer.UploadUrl, "resized.png");
+                                                var StringResponse = new WebClient().Encoding.GetString(response);
+                                                var attachment = api.Photo.SaveMessagesPhoto(StringResponse);
+
+                                                api.Messages.Send(new VkNet.Model.RequestParams.MessagesSendParams()
+                                                {
+                                                    Attachments = attachment,
+                                                    Message = update.MessageNew.Message.Text,
+                                                    PeerId = update.MessageNew.Message.PeerId,
+                                                    RandomId = new Random().Next() * 1000
+                                                });
+
+
+                                                img.Dispose();
+
+                                                resized.Dispose();
+
+                                                File.Delete("photo" + update.MessageNew.Message.Attachments[0].Instance.ToString() + ".png");
+
+                                                Log("Sended.");
+                                            
+                                        }
+
+
+
+                                        api.Messages.MarkAsRead(update.MessageNew.Message.PeerId + "");
+
+                                    }
+                                }
+                            }
+
+                            
+                        }
+                    }
+                }
+                catch (Exception err)
+                {
+
+                    Log(err.Message);
+                    
+                }
+
+
+            }
+
+
+        }
+
+        private void Log(string text)
+        {
+            lines.Add("["+ DateTime.Now +"]: " + text);
+  
+            richTextBox1.Invoke(new Action(() => richTextBox1.Lines = lines.ToArray()));
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.UI = checkBox1.Checked;
+            Properties.Settings.Default.Save();
+            Properties.Settings.Default.Reload();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.AutoStart = checkBox1.Checked;
+            Properties.Settings.Default.Save();
+            Properties.Settings.Default.Reload();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("config.txt"))
+            {
+                Log("config.txt считан.");
+
+                string[] config = File.ReadAllLines("config.txt");
+
+                foreach (var item in config)
+                {
+                  string[] keyPare =  item.Split('=');
+
+                    if (keyPare[0] == "Token")
+                    {
+                        AccessToken = keyPare[1];
+                    }
+                    else
+                    {
+                        GroupId = keyPare[1];
+                    }
+
+                }
+
+                Log("TOKEN=" + AccessToken);
+                Log("ID=" + GroupId);
+                checkBox1.Checked = Properties.Settings.Default.AutoStart;
+                checkBox2.Checked = Properties.Settings.Default.UI;
+
+                if (Properties.Settings.Default.UI == true)
+                {
+                    this.Hide();
+                }
+
+
+                if (Properties.Settings.Default.AutoStart == true)
+                {
+                    Thread BotThr = new Thread(apiBotStart);
+                    GetUpdates = true;
+                    button1.Enabled = false;
+
+                    BotThr.Start();
+                }
+            }
+            else
+            {
+                button1.Enabled = false;
+                Log("Введите токен");
+                textBox1.Text = "Тут";
+            }
+
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Log("Stoped.");
+            button1.Enabled = true;
+            GetUpdates = false;
+            
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (File.Exists("config.txt"))
+                {
+                    string[] filelines = File.ReadAllLines("config.txt");
+                    List<string> listFileLines =  filelines.ToList();
+                    listFileLines.Add("ID="+textBox1.Text);
+                    File.WriteAllLines("config.txt", listFileLines.ToArray());
+                    Log("Id группы записан в config.txt, при следующем запуске будет использован");
+                    Log(textBox1.Text);
+                    Log("Для применения пререзапустите ПО");
+                }
+                else
+                {
+                    var file = File.Create("config.txt");
+                    file.Dispose();
+                    File.WriteAllLines("config.txt", new string[] { "Token=" + textBox1.Text });
+                    Log(textBox1.Text);
+                    Log("Токен записан в config.txt, при следующем запуске будет использован");
+                    Log("Теперь введите id группы");
+                }
+                
+            }
+        }
+    }
+}
